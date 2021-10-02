cee83abfab604151a80747f71cc3ebce24e37f67
Sanjeev Kumar Srivastava <38842661+SanjeevKumarSrivastava@users.noreply.github.com>
2021-10-02 13:53:17 +0530

    update 2 by sanjeev

--- src/main/java/com/jlcindia/springboot/MyBootApplication.java
+++ src/main/java/com/jlcindia/springboot/MyBootApplication.java
@@ -8,6 +8,7 @@ public class MyBootApplication {
 public static void main(String[] args) {
 System.out.println("Starting Boot Application");
 System.out.println("Starting Boot Application update by sanjeev");
+System.out.println("Starting Boot Application update by sanjeev2");
 SpringApplication.run(MyBootApplication.class, args);
 }
 }
